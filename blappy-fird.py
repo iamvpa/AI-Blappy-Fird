592febf751780366b6b8302dfe592cb165d43960
iamvpa <iamvpa@users.noreply.github.com>
2020-07-18 14:32:58 +0530

    added score view

--- blappy-fird.py
+++ blappy-fird.py
@@ -3,6 +3,7 @@ import neat
 import time
 import os
 import random
+pygame.font.init()
 WIN_WIDTH=500
 WIN_HEIGHT=800
 
@@ -11,6 +12,7 @@ BIRD_IMGS= [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird
 PIPE_IMG= pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","pipe.png")))
 BASE_IMG= pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","base.png")))
 BG_IMG= pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bg.png")))
+STAT_FONT=pygame.font.SysFont("comicsans",50)
 
 
 
@@ -105,7 +107,7 @@ class Pipe:
 
         self.top=0
         self.bottom=0
-        self.PIPE_TOP=pygame.trasform.flip(PIPE_IMG,False,True)
+        self.PIPE_TOP=pygame.transform.flip(PIPE_IMG,False,True)
         self.PIPE_BOTTOM=PIPE_IMG
 
         self.passed=False
@@ -143,19 +145,49 @@ class Pipe:
 
 
 class Base:
-    
+    VEL=5
+    WIDTH=BASE_IMG.get_width()
+    IMG=BASE_IMG
+
+    def __init__(self,y):
+        self.y=y
+        self.x1=0
+        self.x2=self.WIDTH
+
+    def move(self):
+        self.x1-=self.VEL
+        self.x2-=self.VEL
+
+        if self.x1+self.WIDTH<0:
+            self.x1=self.x2+self.WIDTH
+
+        if self.x2+self.WIDTH<0:
+            self.x2=self.x1+self.WIDTH
+
+    def draw(self,win):
+        win.blit(self.IMG,(self.x1,self.y))
+        win.blit(self.IMG,(self.x2,self.y))
 
 
 
-def draw_window(win,bird):
+def draw_window(win,bird,pipes,base,score):
     win.blit(BG_IMG,(0,0))
+    for pipe in pipes:
+        pipe.draw(win)
+    text=STAT_FONT.render("Score:"+str(score),1,(255,255,255))
+    win.blit(text,(WIN_WIDTH-10-text.get_width(),10))
+    base.draw(win)
     bird.draw(win)
     pygame.display.update()
 
 def main():
-    bird=Bird(200,200)
+    bird=Bird(230,350)
+    base=Base(730)
+    pipes=[Pipe(600)]
     win=pygame.display.set_mode((WIN_WIDTH,WIN_HEIGHT))
     clock=pygame.time.Clock()
+
+    score=0
     run=True
     while run:
         clock.tick(30)
@@ -163,8 +195,31 @@ def main():
             if event.type==pygame.QUIT:
                 run=False
 
-        bird.move()
-        draw_window(win,bird)
+        #bird.move()
+        add_pipe=False
+        rem=[]
+        for pipe in pipes:
+            if pipe.collide(bird):
+                pass
+            if pipe.x+pipe.PIPE_TOP.get_width()<0:
+                rem.append(pipe)
+            if not pipe.passed and pipe.x<bird.x:
+                pipe.passed=True
+                add_pipe=True
+
+            pipe.move()
+        if add_pipe:
+            score+=1
+            pipes.append(Pipe(600))
+
+        for r in rem:
+            pipes.remove(r)
+
+
+        if bird.y+bird.img.get_height()>730:
+            pass
+        base.move()
+        draw_window(win,bird,pipes,base,score)
     pygame.quit()
     quit()
 main()
