9c932e5149757e10d8a2bafb5e14c5742192919e
iamvpa <iamvpa@users.noreply.github.com>
2020-07-18 13:06:58 +0530

    c2

--- blappy-fird.py
+++ blappy-fird.py
@@ -3,7 +3,7 @@ import neat
 import time
 import os
 import random
-WIN_WIDTH=600
+WIN_WIDTH=500
 WIN_HEIGHT=800
 
 
@@ -96,11 +96,15 @@ def draw_window(win,bird):
 def main():
     bird=Bird(200,200)
     win=pygame.display.set_mode((WIN_WIDTH,WIN_HEIGHT))
+    clock=pygame.time.Clock()
     run=True
     while run:
+        clock.tick(30)
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 run=False
+
+        bird.move()
         draw_window(win,bird)
     pygame.quit()
     quit()
